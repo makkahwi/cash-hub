44460e57c35212b674d56baefdcd2f394204ef20
makkahwi <suhaibahmadai@hotmail.com>
2023-07-02 21:27:19 +0300

    Split GetYours Page Sections

---
+++ pages/get-your-showroom/Header.vue
@@ -0,0 +1,32 @@
+<template>
+  <div
+    class="page-header position-relative"
+    :style="{
+      backgroundImage: 'url(' + headerImg + ')',
+      backgroundSize: 'cover',
+      backgroundPosition: 'center top',
+    }"
+  >
+    <span class="mask bg-gradient-dark opacity-7" />
+
+    <div class="container py-7 pt-8 postion-relative z-index-2">
+      <div class="mx-auto text-center my-4">
+        <h4 class="text-white my-4">Get Your Showroom</h4>
+
+        <p class="text-white">
+          If you are interested to have your own CCS, follow the steps below to
+          get it. Don't worry, even if you are not a developer, you could still
+          build your own copy.
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import headerImg from "@/assets/img/currencies.jpg";
+
+definePageMeta({
+  layout: false,
+});
+</script>
---
+++ pages/get-your-showroom/HowToDo/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container my-6">
+    <div class="mt-8 row">
+      <div class="mx-auto text-center col-md-6">
+        <h3>How To Do</h3>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="mx-auto my-3 mb-5 col-md-11 text-justify">
+        <p>
+          There are two possible options to build your own copy of CCS at the
+          time, and later on we'll offer more options. Both options are about
+          acquiring free-of-charge servers to host your CCS, n then easily
+          deploy the code to the servers to start inserting your own data.
+        </p>
+      </div>
+
+      <div class="mx-auto col-md-5">
+        <Option1 />
+      </div>
+
+      <div class="mx-auto col-md-5">
+        <Option2 />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import Option1 from "./Option1.vue";
+import Option2 from "./Option2.vue";
+
+definePageMeta({
+  layout: false,
+});
+</script>
