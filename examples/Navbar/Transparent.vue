004e91d526898f39fede12e24f77ecbf8ec1f86c
makkahwi <suhaibahmadai@hotmail.com>
2023-07-02 22:24:00 +0300

    Update Navbar UI

--- examples/Navbar/Transparent.vue
+++ examples/Navbar/Transparent.vue
@@ -1,15 +1,15 @@
 <template>
   <nav
-    class="navbar navbar-expand-lg top-0 z-index-3 position-absolute mt-4"
+    class="navbar navbar-expand-lg top-0 z-index-3 position-absolute mt-4 bg-gradient-success rounded-3"
     :class="isBlur ? isBlur : 'shadow-none my-2 navbar-transparent w-100'"
   >
     <div class="container ps-2 pe-0">
       <NuxtLink
-        class="navbar-brand font-weight-bolder ms-lg-0 ms-3"
-        :class="darkMode ? 'text-black' : 'text-white'"
+        class="navbar-brand font-weight-bolder ms-lg-0 ms-3 text-black"
         to="/"
         >CCS</NuxtLink
       >
+
       <button
         class="shadow-none navbar-toggler ms-2"
         type="button"
@@ -25,6 +25,7 @@
           <span class="navbar-toggler-bar bar3"></span>
         </span>
       </button>
+
       <div
         id="navigation"
         class="pt-3 pb-2 collapse navbar-collapse w-100 py-lg-0"
@@ -33,7 +34,7 @@
           <li class="mx-2 nav-item dropdown dropdown-hover">
             <NuxtLink
               to="/landing"
-              class="cursor-pointer nav-link ps-2 d-flex justify-content-between align-items-center"
+              class="cursor-pointer nav-link ps-2 d-flex justify-content-between align-items-center text-dark"
             >
               Home
             </NuxtLink>
@@ -42,7 +43,7 @@
           <li class="mx-2 nav-item dropdown dropdown-hover">
             <NuxtLink
               to="/my-showroom"
-              class="cursor-pointer nav-link ps-2 d-flex justify-content-between align-items-center"
+              class="cursor-pointer nav-link ps-2 d-flex justify-content-between align-items-center text-dark"
             >
               My Showroom
             </NuxtLink>
@@ -51,7 +52,7 @@
           <li class="mx-2 nav-item dropdown dropdown-hover">
             <NuxtLink
               to="/get-your-showroom"
-              class="cursor-pointer nav-link ps-2 d-flex justify-content-between align-items-center"
+              class="cursor-pointer nav-link ps-2 d-flex justify-content-between align-items-center text-dark"
             >
               Get Your Showroom
             </NuxtLink>
@@ -60,7 +61,7 @@
           <li class="mx-2 nav-item dropdown dropdown-hover">
             <NuxtLink
               to="/dashboards/default"
-              class="cursor-pointer nav-link ps-2 d-flex justify-content-between align-items-center"
+              class="cursor-pointer nav-link ps-2 d-flex justify-content-between align-items-center text-dark"
             >
               Dashboard
             </NuxtLink>
@@ -71,8 +72,7 @@
           <li class="nav-item">
             <NuxtLink
               to="/login"
-              class="mb-0 btn btn-sm me-1"
-              :class="btnBackground ? btnBackground : 'bg-white text-dark'"
+              class="mb-0 btn btn-sm me-1 bg-white text-dark"
               onclick="smoothToPricing('pricing-soft-ui')"
             >
               Login
@@ -88,6 +88,7 @@
 import downArrWhite from "@/assets/img/down-arrow-white.svg";
 import downArrBlack from "@/assets/img/down-arrow-dark.svg";
 import bg from "@/assets/img/curved-images/curved8.jpg";
+
 export default {
   name: "Navbar",
   props: {
