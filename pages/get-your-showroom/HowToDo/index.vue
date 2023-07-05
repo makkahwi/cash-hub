a53e69723a4662e9b458f5163e29ceaaeceadef4
makkahwi <suhaibahmadai@hotmail.com>
2023-07-05 10:49:40 +0300

    Apply PageSection To GetYourShowroom Page

--- pages/get-your-showroom/HowToDo/index.vue
+++ pages/get-your-showroom/HowToDo/index.vue
@@ -1,14 +1,8 @@
 <template>
-  <div class="container my-6">
-    <div class="mt-8 row">
-      <div class="mx-auto text-center col-md-6">
-        <h3>How To Do</h3>
-      </div>
-    </div>
-
+  <PageSection title="How To Do">
     <div class="row">
-      <div class="mx-auto my-3 mb-5 col-md-11 text-justify">
-        <p>
+      <div class="col-md-12 mb-5">
+        <p class="text-justify">
           There are two possible options to build your own copy of CCS at the
           time, and later on we'll offer more options. Both options are about
           acquiring free-of-charge servers to host your CCS, n then easily
@@ -16,18 +10,20 @@
         </p>
       </div>
 
-      <div class="mx-auto col-md-5">
+      <div class="col-md-6">
         <Option1 />
       </div>
 
-      <div class="mx-auto col-md-5">
+      <div class="col-md-6">
         <Option2 />
       </div>
     </div>
-  </div>
+  </PageSection>
 </template>
 
+
 <script setup>
+import PageSection from "../../../components/PageSection";
 import Option1 from "./Option1";
 import Option2 from "./Option2";
 
