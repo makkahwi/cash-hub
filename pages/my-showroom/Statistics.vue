1c069df6e7d83faa339ad0b8c0ac2296e41751c2
makkahwi <suhaibahmadai@hotmail.com>
2023-07-05 15:06:36 +0300

    Engage Statistics Into MyShowroom List

--- pages/my-showroom/Statistics.vue
+++ pages/my-showroom/Statistics.vue
@@ -20,65 +20,61 @@
 </template>
 
 <script setup>
-import PageSection from "@/components/PageSection";
-import DefaultCounterCard from "~~/examples/cards/DefaultCounterCard";
+  import PageSection from "@/components/PageSection";
+  import DefaultCounterCard from "~~/examples/cards/DefaultCounterCard";
 
-const statsData = [
-  {
-    title: "Count of Banknotes",
-    value: "554",
-  },
-  {
-    title: "Count of Coins",
-    value: "356",
-  },
-  {
-    title: "Count of Circuable Pieces",
-    value: "455",
-  },
-  {
-    title: "Count of Outdated Pieces",
-    value: "455",
-  },
-  {
-    title: "Count of African Sets",
-    value: "21",
-  },
-  {
-    title: "Count of American Sets",
-    value: "26",
-  },
-  {
-    title: "Count of Asian Sets",
-    value: "99", 
-  },
-  {
-    title: "Count of Europen Sets",
-    value: "24", 
-  },
-  {
-    title: "Count of Oceanian Sets",
-    value: "7", 
-  },
-  {
-    title: "Count of African Pieces",
-    value: "78", 
-  },
-  {
-    title: "Count of American Pieces",
-    value: "60", 
-  },
-  {
-    title: "Count of Asian Pieces",
-    value: "333", 
-  },
-  {
-    title: "Count of Europen Pieces",
-    value: "56", 
-  },
-  {
-    title: "Count of Oceanian Pieces",
-    value: "22", 
-  },
-];
+  const props = defineProps({
+    currencies: {
+      type: Array,
+      default: [],
+    },
+  });
+
+  const statsData = [
+    {
+      title: "Count of Banknotes",
+      value: "554",
+      value: props.currencies.filter(({type}) => type == "Banknote")?.length,
+    },
+    {
+      title: "Count of Coins",
+      value: "356",
+      value: props.currencies.filter(({type}) => type == "Coin")?.length,
+    },
+    {
+      title: "Count of Circuable Pieces",
+      value: "455",
+      value: props.currencies.filter(({mostRecentEdition, order}) => mostRecentEdition == order)?.length,
+    },
+    {
+      title: "Count of Outdated Pieces",
+      value: "455",
+      value: props.currencies.filter(({mostRecentEdition, order}) => mostRecentEdition != order)?.length,
+    },
+    {
+      title: "Count of African Pieces",
+      value: "78", 
+      value: props.currencies.filter(({continent}) => continent == "Africa")?.length,
+    },
+    {
+      title: "Count of American Pieces",
+      value: "60", 
+      value: props.currencies.filter(({continent}) => continent == "America")?.length,
+    },
+    {
+      title: "Count of Asian Pieces",
+      value: "333", 
+      value: props.currencies.filter(({continent}) => continent == "Asia")?.length,
+    },
+    {
+      title: "Count of Europen Pieces",
+      value: "56", 
+      value: props.currencies.filter(({continent}) => continent == "Europe")?.length,
+    },
+    {
+      title: "Count of Oceanian Pieces",
+      value: "22", 
+      value: props.currencies.filter(({continent}) => continent == "Oceania")?.length,
+    },
+  ];
 </script>
