c260399cdad98daa5f6d992e0ba1db8dfc40458d
makkahwi <suhaibahmadai@hotmail.com>
2023-07-05 11:29:52 +0300

    Update Layout UI

--- layouts/Footer/index.vue
+++ layouts/Footer/index.vue
@@ -1,105 +1,101 @@
 <template>
-  <footer class="py-5 footer">
-    <div class="container">
-      <div class="row">
-        <div class="mx-auto mb-4 text-center col-lg-8">
-          <NuxtLink
-            to="/"
-            class="mb-2 text-secondary me-xl-5 mx-2 mb-sm-0"
-          >
-            Home
-          </NuxtLink>
+  <PageSection bg="light" p="5 px-8" m="0 mt-5">
+    <div class="row justify-content-center">
+      <div class="col-lg-8 text-center">
+        <NuxtLink
+          to="/"
+          class="text-secondary mx-2"
+        >
+          Home
+        </NuxtLink>
 
-          <NuxtLink
-            to="/my-showroom"
-            class="mb-2 text-secondary me-xl-5 mx-2 mb-sm-0"
-          >
-            My Showroom
-          </NuxtLink>
+        <NuxtLink
+          to="/my-showroom"
+          class="text-secondary mx-2"
+        >
+          My Showroom
+        </NuxtLink>
 
-          <NuxtLink
-            to="/get-your-showroom"
-            class="mb-2 text-secondary me-xl-5 mx-2 mb-sm-0"
-          >
-            Get Your Showroom
-          </NuxtLink>
-        </div>
-
-        <div class="mx-auto my-4 text-center col-lg-8">
-          <a
-            href="https://www.github.com/arromi.creatives"
-            target="_blank"
-            class="text-secondary me-xl-4 me-4"
-          >
-            <span class="text-lg fab fa-github"></span>
-          </a>
-
-          <a
-            href="https://www.linkedin.com/arromi.creatives"
-            target="_blank"
-            class="text-secondary me-xl-4 me-4"
-          >
-            <span class="text-lg fab fa-linkedin"></span>
-          </a>
-
-          <a
-            href="https://www.facebook.com/arromi.creatives"
-            target="_blank"
-            class="text-secondary me-xl-4 me-4"
-          >
-            <span class="text-lg fab fa-facebook"></span>
-          </a>
-
-          <a
-            href="https://www.instagram.com/arromi.creatives"
-            target="_blank"
-            class="text-secondary me-xl-4 me-4"
-          >
-            <span class="text-lg fab fa-instagram"></span>
-          </a>
-
-          <a
-            href="https://wasap.my/962788424973"
-            target="_blank"
-            class="text-secondary me-xl-4 me-4"
-          >
-            <span class="text-lg fab fa-whatsapp"></span>
-          </a>
-
-          <a
-            href="https://t.me/makkahwi"
-            target="_blank"
-            class="text-secondary me-xl-4 me-4"
-          >
-            <span class="text-lg fab fa-telegram"></span>
-          </a>
-
-          <a
-            href="mailto:arromi.creatives@gmail.com"
-            target="_blank"
-            class="text-secondary me-xl-4 me-4"
-          >
-            <span class="text-lg fa fa-envelope"></span>
-          </a>
-        </div>
+        <NuxtLink
+          to="/get-your-showroom"
+          class="text-secondary mx-2"
+        >
+          Get Your Showroom
+        </NuxtLink>
       </div>
 
-      <div class="row">
-        <div class="mx-auto mt-4 text-center col-lg-8">
-          <p class="mb-0 text-secondary">
-            Crafted with Care by
-            <a href="https://arromi.net/" target="_blank" class="text-secondary"
-              >Arromi Creatives</a
-            >
-            ©
-            {{ new Date().getFullYear() }}
-          </p>
-        </div>
+      <div class="col-lg-8 my-5 text-center">
+        <a
+          href="https://www.github.com/arromi.creatives"
+          target="_blank"
+          class="text-secondary me-4"
+        >
+          <span class="text-lg fab fa-github"></span>
+        </a>
+
+        <a
+          href="https://www.linkedin.com/arromi.creatives"
+          target="_blank"
+          class="text-secondary me-4"
+        >
+          <span class="text-lg fab fa-linkedin"></span>
+        </a>
+
+        <a
+          href="https://www.facebook.com/arromi.creatives"
+          target="_blank"
+          class="text-secondary me-4"
+        >
+          <span class="text-lg fab fa-facebook"></span>
+        </a>
+
+        <a
+          href="https://www.instagram.com/arromi.creatives"
+          target="_blank"
+          class="text-secondary me-4"
+        >
+          <span class="text-lg fab fa-instagram"></span>
+        </a>
+
+        <a
+          href="https://wasap.my/962788424973"
+          target="_blank"
+          class="text-secondary me-4"
+        >
+          <span class="text-lg fab fa-whatsapp"></span>
+        </a>
+
+        <a
+          href="https://t.me/makkahwi"
+          target="_blank"
+          class="text-secondary me-4"
+        >
+          <span class="text-lg fab fa-telegram"></span>
+        </a>
+
+        <a
+          href="mailto:arromi.creatives@gmail.com"
+          target="_blank"
+          class="text-secondary me-4"
+        >
+          <span class="text-lg fa fa-envelope"></span>
+        </a>
+      </div>
+      
+      <div class="col-lg-8">
+        <p class="text-secondary my-0">
+          Crafted with Care by
+          <a href="https://arromi.net/" target="_blank" class="text-secondary"
+            >Arromi Creatives</a
+          >
+          ©
+          {{ new Date().getFullYear() }}
+        </p>
       </div>
     </div>
-  </footer>
+  </PageSection>
 </template>
 
-<script>
-export default { name: "AppFooter" };
+<script setup>
+  import PageSection from "@/components/PageSection"
 </script>
--- layouts/Navbar/index.vue
+++ layouts/Navbar/index.vue
@@ -1,123 +1,87 @@
 <template>
-  <nav
-    class="navbar navbar-expand-lg top-0 z-index-3 position-absolute mt-4 bg-gradient-success rounded-3"
-    :class="isBlur ? isBlur : 'shadow-none my-2 navbar-transparent w-100'"
-  >
-    <div class="container ps-2 pe-0">
-      <NuxtLink
-        class="navbar-brand font-weight-bolder ms-lg-0 ms-3 text-black"
-        to="/"
-        >CCS</NuxtLink
-      >
+  <div class="overflow-hidden">
+    <nav
+      class="navbar navbar-expand-lg top-0 start-3 end-3 z-index-5 position-fixed mt-4 bg-gradient-success rounded-3 shadow-none"
+    >
+      <div class="container">
+        <NuxtLink
+          class="navbar-brand font-weight-bolder text-black"
+          to="/"
+        >
+          CCS
+        </NuxtLink>
 
-      <button
-        class="shadow-none navbar-toggler ms-2"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navigation"
-        aria-controls="navigation"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="mt-2 navbar-toggler-icon">
-          <span class="navbar-toggler-bar bar1"></span>
-          <span class="navbar-toggler-bar bar2"></span>
-          <span class="navbar-toggler-bar bar3"></span>
-        </span>
-      </button>
+        <button
+          class="shadow-none navbar-toggler ms-2"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navigation"
+          aria-controls="navigation"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="mt-2 navbar-toggler-icon">
+            <span class="navbar-toggler-bar bar1"></span>
+            <span class="navbar-toggler-bar bar2"></span>
+            <span class="navbar-toggler-bar bar3"></span>
+          </span>
+        </button>
 
-      <div
-        id="navigation"
-        class="pt-3 pb-2 collapse navbar-collapse w-100 py-lg-0"
-      >
-        <ul class="mx-auto navbar-nav navbar-nav-hover">
-          <li class="mx-2 nav-item dropdown dropdown-hover">
-            <NuxtLink
-              to="/"
-              class="cursor-pointer nav-link ps-2 d-flex justify-content-between align-items-center text-dark"
-            >
-              Home
-            </NuxtLink>
-          </li>
+        <div
+          id="navigation"
+          class="pt-3 pb-2 collapse navbar-collapse w-100 py-lg-0"
+        >
+          <ul class="mx-auto navbar-nav navbar-nav-hover">
+            <li class="mx-2 nav-item dropdown dropdown-hover">
+              <NuxtLink
+                to="/"
+                class="cursor-pointer nav-link ps-2 d-flex justify-content-between align-items-center text-dark"
+              >
+                Home
+              </NuxtLink>
+            </li>
 
-          <li class="mx-2 nav-item dropdown dropdown-hover">
-            <NuxtLink
-              to="/my-showroom"
-              class="cursor-pointer nav-link ps-2 d-flex justify-content-between align-items-center text-dark"
-            >
-              My Showroom
-            </NuxtLink>
-          </li>
+            <li class="mx-2 nav-item dropdown dropdown-hover">
+              <NuxtLink
+                to="/my-showroom"
+                class="cursor-pointer nav-link ps-2 d-flex justify-content-between align-items-center text-dark"
+              >
+                My Showroom
+              </NuxtLink>
+            </li>
 
-          <li class="mx-2 nav-item dropdown dropdown-hover">
-            <NuxtLink
-              to="/get-your-showroom"
-              class="cursor-pointer nav-link ps-2 d-flex justify-content-between align-items-center text-dark"
-            >
-              Get Your Showroom
-            </NuxtLink>
-          </li>
+            <li class="mx-2 nav-item dropdown dropdown-hover">
+              <NuxtLink
+                to="/get-your-showroom"
+                class="cursor-pointer nav-link ps-2 d-flex justify-content-between align-items-center text-dark"
+              >
+                Get Your Showroom
+              </NuxtLink>
+            </li>
 
-          <li class="mx-2 nav-item dropdown dropdown-hover">
-            <NuxtLink
-              to="/dashboards/default"
-              class="cursor-pointer nav-link ps-2 d-flex justify-content-between align-items-center text-dark"
-            >
-              Dashboard
-            </NuxtLink>
-          </li>
-        </ul>
+            <li class="mx-2 nav-item dropdown dropdown-hover">
+              <NuxtLink
+                to="/dashboards/default"
+                class="cursor-pointer nav-link ps-2 d-flex justify-content-between align-items-center text-dark"
+              >
+                Dashboard
+              </NuxtLink>
+            </li>
+          </ul>
 
-        <ul class="navbar-nav d-lg-block d-none">
-          <li class="nav-item">
-            <NuxtLink
-              to="/login"
-              class="mb-0 btn btn-sm me-1 bg-white text-dark"
-              onclick="smoothToPricing('pricing-soft-ui')"
-            >
-              Login
-            </NuxtLink>
-          </li>
-        </ul>
+          <ul class="navbar-nav d-lg-block d-none">
+            <li class="nav-item">
+              <NuxtLink
+                to="/login"
+                class="mb-0 btn btn-sm me-1 bg-white text-dark"
+                onclick="smoothToPricing('pricing-soft-ui')"
+              >
+                Login
+              </NuxtLink>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
-  </nav>
+    </nav>
+  </div>
 </template>
-
-<script>
-import downArrWhite from "@/assets/img/down-arrow-white.svg";
-import downArrBlack from "@/assets/img/down-arrow-dark.svg";
-import bg from "@/assets/img/curved-images/curved8.jpg";
-
-export default {
-  name: "Navbar",
-  props: {
-    btnBackground: {
-      type: String,
-      default: "",
-    },
-    isBlur: {
-      type: String,
-      default: "",
-    },
-    darkMode: {
-      type: Boolean,
-      default: false,
-    },
-  },
-  data() {
-    return {
-      downArrWhite,
-      downArrBlack,
-      bg,
-    };
-  },
-  computed: {
-    darkModes() {
-      return {
-        "text-dark": this.darkMode,
-      };
-    },
-  },
-};
-</script>
