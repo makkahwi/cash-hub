fe475451c0a65aba01075535c65ed5eda41b674d
makkahwi <suhaibahmadai@hotmail.com>
2023-07-10 13:22:25 +0300

    Update FeatureWork Section

--- pages/landing/FutureWork.vue
+++ pages/landing/FutureWork.vue
@@ -1,11 +1,26 @@
 <template>
+  <PageSection bg="success" color="white" title="Coming Soon Features">
+    <h5 class="text-white">
+      As of this moment, you could use the admin panel to add the currencies collection and the public showroom to view them. Soon, a new admin panel will be built to not only include CRUD controls, but also a dashboard to provide reports & data analysis about the collection.
+    </h5>
+  </PageSection>
+  
   <PageSection bg="light" title="Future Works">
-    <p>
-      This is an open-source project with potential to expand its uses (like
-      any other sort of collection showrooming or even other uses like photos
-      gallery) and get fund to turn it into a profitable project (like turning
-      it into a SaaS product).
-    </p>
+    <h5 class="text-justify">
+      <ul>
+        <li>
+          Now user is required to upload an image of their collected currency piece. Later an AI-based feature will be built to automatically find the appropriate photo for the piece to attach.
+        </li>
+
+        <li>
+          Expand the project uses (like any other sort of collections showrooming or even other uses like photos gallery)
+        </li>
+
+        <li>
+          This is an open-source project with potential to get fund & turn it into a profitable project (like turning into a SaaS product).
+        </li>
+      </ul>
+    </h5>
   </PageSection>
 </template>
 
