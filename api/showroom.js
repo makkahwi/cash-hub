a94c4f52a87f0c2232aee4229aa75a9f2440bb45
makkahwi <suhaibahmadai@hotmail.com>
2023-07-10 13:03:42 +0300

    Build getCurrencies API Filters Engagement

--- api/showroom.js
+++ api/showroom.js
@@ -1,6 +1,16 @@
 const baseUrl = "http://localhost:1337/api";
 
-export const getCurrencies = (filters) => useFetch(`${baseUrl}/currencies`);
+export const getCurrencies = (filters) => {
+  const finalFilters = [];
+
+  const addArrays = (list,key) => list[key].forEach((value,i) => finalFilters.push(`filters[${key}][$eq][${i}]=${value}`));
+
+  Object.keys(filters).forEach(key => 
+    typeof filters[key] === "object" ? addArrays(filters, key) : finalFilters.push(`filters[${key}][${key === "startDate" ? '$gte' : "$lte"}]=${filters[key]}`)
+    );
+
+  return useFetch(`${baseUrl}/currencies?${finalFilters.join("&")}`);
+};
 
 export const getCurrency = (id) => useFetch(`${baseUrl}/currencies/${id}`);
 
