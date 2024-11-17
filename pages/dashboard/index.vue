09ac1dbc731aa0988d498927b5e655440f4a31bd
makkahwi <suhaibahmadai@hotmail.com>
2024-11-17 15:25:22 +0300

    Update Dashboard UI

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -2,203 +2,243 @@
   <div>
     <PageHeader
       title="Create New Currency Record"
-      subtitle="Fulfill inputs below please"
+      subtitle="Fill in the details to add a new currency"
     />
 
-    <div class="p-6 bg-white card border-radius-xl">
-      <h5 class="font-weight-bolder">New Currency Information</h5>
-
+    <div class="p-6 bg-light m-0">
       <form @submit.prevent="handleSubmit">
-        <div class="mt-3 row">
-          <div class="col-sm-6 col-md-3">
-            <label>Continent</label>
-            <select v-model="formData.continent" class="form-control" required>
-              <option value="" disabled>Select Continent</option>
-              <option value="Africa">Africa</option>
-              <option value="Asia">Asia</option>
-              <option value="Europe">Europe</option>
-              <option value="North America">North America</option>
-              <option value="South America">South America</option>
-              <option value="Australia">Australia</option>
-              <option value="Antarctica">Antarctica</option>
-            </select>
-          </div>
+        <div class="row g-4">
+          <!-- Zone Information -->
+          <fieldset class="col-12">
+            <legend class="text-muted fs-6">Zone Information</legend>
+            <div class="row g-3">
+              <div class="col-md-4">
+                <label for="continent" class="form-label">Continent</label>
+                <select
+                  v-model="formData.continent"
+                  class="form-select"
+                  id="continent"
+                  required
+                >
+                  <option value="" disabled>Select Continent</option>
+                  <option value="Africa">Africa</option>
+                  <option value="Asia">Asia</option>
+                  <option value="Europe">Europe</option>
+                  <option value="North America">North America</option>
+                  <option value="South America">South America</option>
+                  <option value="Australia">Australia</option>
+                  <option value="Antarctica">Antarctica</option>
+                </select>
+              </div>
+              <div class="col-md-4">
+                <label for="zoneName" class="form-label">Zone Name</label>
+                <select
+                  v-model="formData.zoneName"
+                  class="form-select"
+                  id="zoneName"
+                  required
+                >
+                  <option value="" disabled>Select Zone</option>
+                  <option
+                    v-for="zone in currencyZones"
+                    :key="zone.mapCode"
+                    :value="zone.name"
+                  >
+                    {{ zone.name }}
+                  </option>
+                </select>
+              </div>
+              <div class="col-md-4">
+                <label for="zoneFullName" class="form-label"
+                  >Zone Full Name</label
+                >
+                <input
+                  v-model="formData.zoneFullName"
+                  class="form-control"
+                  id="zoneFullName"
+                  type="text"
+                  placeholder="e.g., The Hashemite Kingdom of Jordan"
+                  required
+                />
+              </div>
+            </div>
+          </fieldset>
 
-          <div class="col-sm-6 col-md-3">
-            <label>Zone Name</label>
-            <select v-model="formData.zoneName" class="form-control" required>
-              <option value="" disabled>Select Zone</option>
-              <option
-                v-for="zone in currencyZones"
-                :key="zone.mapCode"
-                :value="zone.name"
-              >
-                {{ zone.name }}
-              </option>
-            </select>
-          </div>
+          <!-- Currency Details -->
+          <fieldset class="col-12">
+            <legend class="text-muted fs-6">Currency Details</legend>
+            <div class="row g-3">
+              <div class="col-md-4">
+                <label for="currencyName" class="form-label"
+                  >Currency Name</label
+                >
+                <input
+                  v-model="formData.name"
+                  class="form-control"
+                  id="currencyName"
+                  type="text"
+                  placeholder="e.g., Dinar"
+                  required
+                />
+              </div>
+              <div class="col-md-4">
+                <label for="currencyCode" class="form-label"
+                  >Currency Code</label
+                >
+                <input
+                  v-model="formData.code"
+                  class="form-control"
+                  id="currencyCode"
+                  type="text"
+                  placeholder="e.g., JOD"
+                  required
+                />
+              </div>
+              <div class="col-md-4">
+                <label for="fractionName" class="form-label"
+                  >Fraction Name</label
+                >
+                <input
+                  v-model="formData.fractionName"
+                  class="form-control"
+                  id="fractionName"
+                  type="text"
+                  placeholder="e.g., Piastre"
+                  required
+                />
+              </div>
+              <div class="col-md-4">
+                <label for="fractionSize" class="form-label"
+                  >Fraction Size</label
+                >
+                <input
+                  v-model="formData.fractionSize"
+                  class="form-control"
+                  id="fractionSize"
+                  type="number"
+                  placeholder="e.g., 100"
+                  required
+                />
+              </div>
+              <div class="col-md-4">
+                <label for="edition" class="form-label">Edition No.</label>
+                <input
+                  v-model="formData.edition"
+                  class="form-control"
+                  id="edition"
+                  type="number"
+                  placeholder="e.g., 5"
+                />
+              </div>
+              <div class="col-md-4">
+                <label for="year" class="form-label">Edition Year</label>
+                <input
+                  v-model="formData.year"
+                  class="form-control"
+                  id="year"
+                  type="number"
+                  placeholder="e.g., 2022"
+                />
+              </div>
+            </div>
+          </fieldset>
 
-          <div class="col-sm-6 col-md-3">
-            <label>Zone Full Name</label>
-            <input
-              v-model="formData.zoneFullName"
-              class="form-control"
-              type="text"
-              placeholder="eg. The Hashemite Kingdom of Jordan"
-              required
-            />
-          </div>
+          <!-- Additional Details -->
+          <fieldset class="col-12">
+            <legend class="text-muted fs-6">Additional Details</legend>
+            <div class="row g-3">
+              <div class="col-md-4">
+                <label for="circability" class="form-label">Circability</label>
+                <select
+                  v-model="formData.status"
+                  class="form-select"
+                  id="circability"
+                  required
+                >
+                  <option value="" disabled>Select Status</option>
+                  <option value="Circulable">Circulable</option>
+                  <option value="Outdated">Outdated</option>
+                </select>
+              </div>
+              <div class="col-md-4">
+                <label for="type" class="form-label">Type</label>
+                <select
+                  v-model="formData.type"
+                  class="form-select"
+                  id="type"
+                  required
+                >
+                  <option value="" disabled>Select Type</option>
+                  <option value="Banknote">Banknote</option>
+                  <option value="Coin">Coin</option>
+                </select>
+              </div>
+              <div class="col-md-4">
+                <label for="value" class="form-label">Value</label>
+                <input
+                  v-model="formData.value"
+                  class="form-control"
+                  id="value"
+                  type="number"
+                  placeholder="e.g., 20"
+                  required
+                />
+              </div>
+              <div class="col-md-4">
+                <label for="date" class="form-label">Collection Date</label>
+                <input
+                  v-model="formData.date"
+                  class="form-control"
+                  id="date"
+                  type="date"
+                />
+              </div>
+              <div class="col-md-4">
+                <label for="usdToLocal" class="form-label">Value Per USD</label>
+                <input
+                  v-model="formData.usdToLocal"
+                  class="form-control"
+                  id="usdToLocal"
+                  type="number"
+                  step="0.01"
+                  placeholder="e.g., 0.71"
+                  required
+                />
+              </div>
+            </div>
+          </fieldset>
 
-          <div class="col-12 col-sm-6 col-md-3">
-            <label>Currency Name</label>
-            <input
-              v-model="formData.name"
-              class="form-control"
-              type="text"
-              placeholder="eg. Dinar"
-              required
-            />
-          </div>
-
-          <div class="mt-3 col-12 col-sm-6 col-md-3 mt-sm-0">
-            <label>Currency Code</label>
-            <input
-              v-model="formData.code"
-              class="form-control"
-              type="text"
-              placeholder="eg. JOD"
-              required
-            />
-          </div>
-
-          <div class="col-sm-6 col-md-3">
-            <label>Fraction Name</label>
-            <input
-              v-model="formData.fractionName"
-              class="form-control"
-              type="text"
-              placeholder="eg. Piastre"
-              required
-            />
-          </div>
-
-          <div class="col-sm-6 col-md-3">
-            <label>Fraction Size</label>
-            <input
-              v-model="formData.fractionSize"
-              class="form-control"
-              type="number"
-              placeholder="eg. 100"
-              required
-            />
-          </div>
-
-          <div class="col-sm-6 col-md-3">
-            <label>Edition No.</label>
-            <input
-              v-model="formData.edition"
-              class="form-control"
-              type="number"
-              placeholder="eg. 5"
-            />
-          </div>
-
-          <div class="col-sm-6 col-md-3">
-            <label>Edition Issue Year</label>
-            <input
-              v-model="formData.year"
-              class="form-control"
-              type="number"
-              placeholder="eg. 2022"
-            />
-          </div>
-
-          <div class="col-sm-6 col-md-3">
-            <label>Circability</label>
-            <select v-model="formData.status" class="form-control" required>
-              <option value="" disabled>Select Status</option>
-              <option
-                v-for="zone in ['Circulable', 'Outdated']"
-                :key="zone"
-                :value="zone"
-              >
-                {{ zone }}
-              </option>
-            </select>
-          </div>
-
-          <div class="col-sm-6 col-md-3">
-            <label>Type</label>
-            <select v-model="formData.type" class="form-control" required>
-              <option value="" disabled>Select Type</option>
-              <option value="Banknote">Banknote</option>
-              <option value="Coin">Coin</option>
-            </select>
-          </div>
-
-          <div class="col-sm-6 col-md-3">
-            <label>Value</label>
-            <input
-              v-model="formData.value"
-              class="form-control"
-              type="number"
-              placeholder="eg. 20"
-              required
-            />
-          </div>
-
-          <div class="col-sm-6 col-md-3">
-            <label>Collection Date</label>
-            <input
-              v-model="formData.date"
-              class="form-control"
-              type="date"
-              placeholder="eg. Jan 2023"
-            />
-          </div>
-
-          <div class="col-sm-6 col-md-3">
-            <label>Value Per USD</label>
-            <input
-              v-model="formData.usdToLocal"
-              class="form-control"
-              type="number"
-              step="0.01"
-              placeholder="eg. 0.71"
-              required
-            />
-          </div>
-
-          <div class="col-sm-6 col-md-3">
-            <label>Front Photo URL</label>
-            <input
-              v-model="formData.fPhoto"
-              class="form-control"
-              type="url"
-              placeholder="Front photo URL"
-            />
-          </div>
-
-          <div class="col-sm-6 col-md-3">
-            <label>Back Photo URL</label>
-            <input
-              v-model="formData.bPhoto"
-              class="form-control"
-              type="url"
-              placeholder="Back photo URL"
-            />
-          </div>
+          <!-- Photos -->
+          <fieldset class="col-12">
+            <legend class="text-muted fs-6">Photos</legend>
+            <div class="row g-3">
+              <div class="col-md-6">
+                <label for="fPhoto" class="form-label">Front Photo URL</label>
+                <input
+                  v-model="formData.fPhoto"
+                  class="form-control"
+                  id="fPhoto"
+                  type="url"
+                  placeholder="Front photo URL"
+                />
+              </div>
+              <div class="col-md-6">
+                <label for="bPhoto" class="form-label">Back Photo URL</label>
+                <input
+                  v-model="formData.bPhoto"
+                  class="form-control"
+                  id="bPhoto"
+                  type="url"
+                  placeholder="Back photo URL"
+                />
+              </div>
+            </div>
+          </fieldset>
         </div>
 
-        <div class="mt-4 button-row d-flex col-12">
-          <ArgonButton
-            type="submit"
-            color="dark"
-            variant="gradient"
-            class="mb-0 ms-auto"
-          >
-            Submit
-          </ArgonButton>
+        <!-- Submit Button -->
+        <div class="mt-4 text-end">
+          <button type="submit" class="btn btn-primary">Submit</button>
         </div>
       </form>
     </div>
