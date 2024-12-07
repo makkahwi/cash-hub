d2326dbf2021c164008f35311be9e9513e62978a
Makkahwi <SuhaibAhmadAi@hotmail.com>
2024-12-07 10:11:33 +0300

    Fix Navbar Responsiveness

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -39,6 +39,11 @@ export default defineNuxtConfig({
           src: "https://buttons.github.io/buttons.js",
           async: true,
         },
+        {
+          src: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js",
+          type: "text/javascript",
+          defer: true,
+        },
       ],
     },
   },
