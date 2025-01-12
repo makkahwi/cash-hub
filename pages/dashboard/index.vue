7290d88c6b0da29906969ab6281c5f6ae76f2bc8
Makkahwi <SuhaibAhmadAi@hotmail.com>
2025-01-12 22:33:46 +0300

    Build Incomplete JSON Data Integration @ Collection Form

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -7,223 +7,96 @@
   <div class="p-6 bg-light m-0">
     <form @submit.prevent="handleSubmit">
       <div class="row g-4">
-        <!-- Zone Information -->
-        <fieldset class="col-12">
-          <legend class="text-muted fs-6">Zone Information</legend>
+        <fieldset
+          v-for="({ title, inputs }, i) of formInputs()"
+          class="col-12"
+          :key="i"
+        >
+          <legend class="text-muted fs-6">{{ title }}</legend>
+
           <div class="row g-3">
-            <div class="col-md-4">
-              <label for="continent" class="form-label">Continent</label>
-              <select
-                v-model="formData.continent"
-                class="form-select"
-                id="continent"
-                required
+            <div
+              v-for="(
+                { label, name, type, options, required, fullWidth, ...rest }, y
+              ) of inputs"
+              :key="y"
+              :class="`col-md-${type == 'checklist-photo' || fullWidth ? 12 : 4}`"
+            >
+              <label for="continent" class="form-label"
+                >{{ label
+                }}<span class="text-danger">{{
+                  required ? " *" : ""
+                }}</span></label
               >
-                <option value="" disabled>Select Continent</option>
-                <option value="Africa">Africa</option>
-                <option value="Asia">Asia</option>
-                <option value="Europe">Europe</option>
-                <option value="North America">North America</option>
-                <option value="South America">South America</option>
-                <option value="Australia">Australia</option>
-                <option value="Antarctica">Antarctica</option>
-              </select>
-            </div>
-            <div class="col-md-4">
-              <label for="zoneName" class="form-label">Zone Name</label>
+
               <select
-                v-model="formData.zoneName"
+                v-model="formData[name]"
+                v-if="type == 'select'"
                 class="form-select"
-                id="zoneName"
-                required
+                :required="required"
+                v-bind="rest"
               >
-                <option value="" disabled>Select Zone</option>
-                <option
-                  v-for="zone in currencyZones"
-                  :key="zone.mapCode"
-                  :value="zone.name"
-                >
-                  {{ zone.name }}
+                <option value="" disabled>Select {{ label }}...</option>
+                <option v-for="{ label, value } of options" :value="value">
+                  {{ label || value }}
                 </option>
               </select>
-            </div>
-            <div class="col-md-4">
-              <label for="zoneFullName" class="form-label"
-                >Zone Full Name</label
-              >
-              <input
-                v-model="formData.zoneFullName"
-                class="form-control"
-                id="zoneFullName"
-                type="text"
-                placeholder="e.g., The Hashemite Kingdom of Jordan"
-                required
-              />
-            </div>
-          </div>
-        </fieldset>
 
-        <!-- Currency Details -->
-        <fieldset class="col-12">
-          <legend class="text-muted fs-6">Currency Details</legend>
-          <div class="row g-3">
-            <div class="col-md-4">
-              <label for="currencyName" class="form-label">Currency Name</label>
-              <input
-                v-model="formData.name"
-                class="form-control"
-                id="currencyName"
-                type="text"
-                placeholder="e.g., Dinar"
-                required
-              />
-            </div>
-            <div class="col-md-4">
-              <label for="currencyCode" class="form-label">Currency Code</label>
-              <input
-                v-model="formData.code"
-                class="form-control"
-                id="currencyCode"
-                type="text"
-                placeholder="e.g., JOD"
-                required
-              />
-            </div>
-            <div class="col-md-4">
-              <label for="fractionName" class="form-label">Fraction Name</label>
-              <input
-                v-model="formData.fractionName"
-                class="form-control"
-                id="fractionName"
-                type="text"
-                placeholder="e.g., Piastre"
-                required
-              />
-            </div>
-            <div class="col-md-4">
-              <label for="fractionSize" class="form-label">Fraction Size</label>
-              <input
-                v-model="formData.fractionSize"
-                class="form-control"
-                id="fractionSize"
-                type="number"
-                placeholder="e.g., 100"
-                required
-              />
-            </div>
-            <div class="col-md-4">
-              <label for="edition" class="form-label">Edition No.</label>
-              <input
-                v-model="formData.edition"
-                class="form-control"
-                id="edition"
-                type="number"
-                placeholder="e.g., 5"
-              />
-            </div>
-            <div class="col-md-4">
-              <label for="year" class="form-label">Edition Year</label>
-              <input
-                v-model="formData.year"
-                class="form-control"
-                id="year"
-                type="number"
-                placeholder="e.g., 2022"
-              />
-            </div>
-          </div>
-        </fieldset>
+              <div v-else-if="type == 'checklist-photo'" class="row my-3">
+                <h6
+                  v-if="
+                    formData.collectedCurrencies.length > 0 ||
+                    (formData.zoneName && formData.type && formData.value)
+                  "
+                >
+                  Select {{ label }}...
+                </h6>
 
-        <!-- Additional Details -->
-        <fieldset class="col-12">
-          <legend class="text-muted fs-6">Additional Details</legend>
-          <div class="row g-3">
-            <div class="col-md-4">
-              <label for="circability" class="form-label">Circability</label>
-              <select
-                v-model="formData.status"
-                class="form-select"
-                id="circability"
-                required
-              >
-                <option value="" disabled>Select Status</option>
-                <option value="Circulable">Circulable</option>
-                <option value="Outdated">Outdated</option>
-              </select>
-            </div>
-            <div class="col-md-4">
-              <label for="type" class="form-label">Type</label>
-              <select
-                v-model="formData.type"
-                class="form-select"
-                id="type"
-                required
-              >
-                <option value="" disabled>Select Type</option>
-                <option value="Banknote">Banknote</option>
-                <option value="Coin">Coin</option>
-              </select>
-            </div>
-            <div class="col-md-4">
-              <label for="value" class="form-label">Value</label>
-              <input
-                v-model="formData.value"
-                class="form-control"
-                id="value"
-                type="number"
-                placeholder="e.g., 20"
-                required
-              />
-            </div>
-            <div class="col-md-4">
-              <label for="date" class="form-label">Collection Date</label>
-              <input
-                v-model="formData.date"
-                class="form-control"
-                id="date"
-                type="date"
-              />
-            </div>
-            <div class="col-md-4">
-              <label for="usdToLocal" class="form-label">Value Per USD</label>
-              <input
-                v-model="formData.usdToLocal"
-                class="form-control"
-                id="usdToLocal"
-                type="number"
-                step="0.01"
-                placeholder="e.g., 0.71"
-                required
-              />
-            </div>
-          </div>
-        </fieldset>
+                <div
+                  v-if="
+                    formData.collectedCurrencies.length > 0 ||
+                    (formData.zoneName && formData.type && formData.value)
+                  "
+                  v-for="{ img, label, value } of options"
+                  :key="value"
+                  class="col-12 col-sm-6 col-md-4 col-lg-2 text-center"
+                >
+                  <img :src="img" class="mb-3" />
 
-        <!-- Photos -->
-        <fieldset class="col-12">
-          <legend class="text-muted fs-6">Photos</legend>
-          <div class="row g-3">
-            <div class="col-md-6">
-              <label for="fPhoto" class="form-label">Front Photo URL</label>
+                  <br />
+
+                  <div>
+                    <input
+                      class="form-check-input me-2"
+                      type="checkbox"
+                      :value="value"
+                      :checked="formData[name].includes(value)"
+                      @change="updateSelection(value)"
+                    />
+
+                    <label class="form-check-label">
+                      {{ label || value }}</label
+                    >
+                  </div>
+                </div>
+
+                <h6 v-else>
+                  Select Required Fields Above For This To Show Options
+                </h6>
+              </div>
+
               <input
-                v-model="formData.fPhoto"
+                v-model="formData[name]"
+                v-else
                 class="form-control"
-                id="fPhoto"
-                type="url"
-                placeholder="Front photo URL"
-              />
-            </div>
-            <div class="col-md-6">
-              <label for="bPhoto" class="form-label">Back Photo URL</label>
-              <input
-                v-model="formData.bPhoto"
-                class="form-control"
-                id="bPhoto"
-                type="url"
-                placeholder="Back photo URL"
+                :type="type || 'text'"
+                :required="required"
+                v-bind="rest"
               />
             </div>
           </div>
+
+          <hr class="mt-5 mb-3" />
         </fieldset>
       </div>
 
@@ -236,34 +109,172 @@
 </template>
 
 <script setup>
-import { reactive } from "vue";
+import { reactive, watch } from "vue";
 import PageHeader from "@/components/PageHeader";
 import { addCurrency } from "~~/api/showroom";
 import { currencyZones } from "~~/utils/consts";
+import countriesData from "../../api/countries.json";
+import currenciesData from "../../api/currencies.json";
+import countriesWithContinents from "../../api/countriesWithContinents.json";
 
-const today = new Date();
-const defaultDate = ref(today.toISOString().split("T")[0]);
+const updateSelection = (value) => {
+  const selectedArray = this.formData.collectedCurrencies;
+  if (selectedArray.includes(value)) {
+    // Remove the value if it's already selected
+    this.formData.collectedCurrencies = selectedArray.filter(
+      (item) => item !== value
+    );
+  } else {
+    // Add the value if it's not already selected
+    this.formData.collectedCurrencies.push(value);
+  }
+};
 
 const defaultFormValues = {
   continent: "",
   zoneName: "",
-  zoneFullName: "",
-  name: "",
-  code: "",
-  fractionName: "",
-  fractionSize: null,
-  edition: null,
-  year: null,
-  status: "",
   type: "",
-  value: null,
-  date: defaultDate,
-  usdToLocal: null,
-  fPhoto: "",
-  bPhoto: "",
+  value: "",
+  collectedCurrencies: [],
+  date: new Date().toISOString().split("T")[0],
+  price: 0,
+  note: "",
 };
 
 const formData = reactive(defaultFormValues);
+
+watch(
+  () => formData.continent,
+  (newContinent) => {
+    formData.zoneName = "";
+    formData.value = "";
+  }
+);
+
+watch(
+  () => formData.zoneName,
+  (newZoneName) => {
+    formData.value = "";
+  }
+);
+
+const formInputs = () => [
+  {
+    title: "Zone Information",
+    inputs: [
+      {
+        name: "continent",
+        label: "Continent",
+        required: true,
+        type: "select",
+        options: countriesWithContinents
+          .reduce(
+            (final, { continent }) =>
+              final.find((already) => already.value == continent)
+                ? final
+                : [...final, { value: continent }],
+            []
+          )
+          .sort((a, b) => (a.value < b.value ? -1 : 1)),
+      },
+      {
+        name: "zoneName",
+        label: "Zone Name",
+        required: true,
+        type: "select",
+        options: formData.continent
+          ? countriesWithContinents
+              .reduce(
+                (final, { name, continent }) =>
+                  final.find((already) => already.value == name)
+                    ? final
+                    : [...final, { value: name, continent }],
+                []
+              )
+              .filter(({ continent }) => continent == formData.continent)
+              .sort((a, b) => (a.value < b.value ? -1 : 1))
+          : [],
+      },
+    ],
+  },
+  {
+    title: "Currency Details",
+    inputs: [
+      {
+        name: "type",
+        label: "Currency Type",
+        required: true,
+        type: "select",
+        options: [{ value: "Banknote" }, { value: "Coin" }],
+      },
+      {
+        name: "value",
+        label: "Currency Value",
+        required: true,
+        type: "select",
+        options:
+          formData.type && formData.zoneName
+            ? currenciesData
+                .filter(
+                  ({ type }) =>
+                    type.toLowerCase() == formData.type.toLowerCase()
+                )
+                .reduce(
+                  (final, { value }) =>
+                    final.find((already) => already.value == value)
+                      ? final
+                      : [...final, { value }],
+                  []
+                )
+            : [],
+      },
+    ],
+  },
+  {
+    title: "Select The Currency",
+    inputs: [
+      {
+        name: "collectedCurrencies",
+        label: "Currencies To Add",
+        required: true,
+        type: "checklist-photo",
+        options: currenciesData
+          .filter(
+            ({ type, value, id }) =>
+              formData.collectedCurrencies.includes(id) ||
+              (type.toLowerCase() == formData.type.toLowerCase() &&
+                value.toLowerCase() == formData.value.toLowerCase())
+          )
+          .map(({ id, name, front_image }) => ({
+            img: front_image,
+            value: id,
+            label: name,
+          })),
+      },
+    ],
+  },
+  {
+    title: "Additional Details",
+    inputs: [
+      {
+        name: "date",
+        label: "Purchase Date",
+        type: "date",
+      },
+      {
+        name: "price",
+        label: "Purchase Price",
+        type: "number",
+        step: 0.01,
+      },
+      {
+        name: "note",
+        label: "Note",
+        type: "text",
+      },
+    ],
+  },
+];
 
 const handleSubmit = () => {
   addCurrency(formData);
