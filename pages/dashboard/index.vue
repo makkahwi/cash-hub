2a0bc11355d3fa703c478f072a960b39d4a5a245
Makkahwi <SuhaibAhmadAi@hotmail.com>
2025-01-21 20:12:36 +0300

    Add Selection View To Currencies Dashboard

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -17,7 +17,17 @@
           <div class="row g-3">
             <div
               v-for="(
-                { label, name, type, options, required, fullWidth, ...rest }, y
+                {
+                  label,
+                  name,
+                  type,
+                  options,
+                  allOptions,
+                  required,
+                  fullWidth,
+                  ...rest
+                },
+                y
               ) of inputs"
               :key="y"
               :class="`col-md-${type == 'checklist-photo' || fullWidth ? 12 : 4}`"
@@ -42,59 +52,98 @@
                 </option>
               </select>
 
-              <div v-else-if="type == 'checklist-photo'" class="row my-3">
-                <h6
-                  v-if="
-                    formData.collectedCurrencies.length > 0 ||
-                    (formData.zoneName && formData.type && formData.value)
-                  "
-                >
-                  Select {{ label }}...
-                </h6>
+              <div v-else-if="type == 'checklist-photo'">
+                <div class="row my-3">
+                  <h6
+                    v-if="formData.zoneName && formData.type && formData.value"
+                  >
+                    Select {{ label }}...
+                  </h6>
 
-                <div
-                  v-if="
-                    formData.collectedCurrencies.length > 0 ||
-                    (formData.zoneName && formData.type && formData.value)
-                  "
-                  v-for="{
-                    front_image,
-                    back_image,
-                    label,
-                    value,
-                    issue_start_year,
-                    issue_end_year,
-                  } of options"
-                  :key="value"
-                  class="col-12 col-sm-6 col-md-4 col-lg-2 text-center"
-                >
-                  <img :src="front_image" class="mb-3" />
-                  <img :src="back_image" class="mb-3" />
+                  <div
+                    v-if="formData.zoneName && formData.type && formData.value"
+                    v-for="{
+                      front_image,
+                      back_image,
+                      label,
+                      value,
+                      issue_start_year,
+                      issue_end_year,
+                    } of options"
+                    :key="value"
+                    class="col-12 col-sm-6 col-md-4 col-lg-2 text-center"
+                  >
+                    <img :src="front_image" class="mb-3" />
+                    <img :src="back_image" class="mb-3" />
 
-                  <br />
+                    <br />
 
-                  <h6>{{ issue_start_year }} - {{ issue_end_year }}</h6>
+                    <h6>{{ issue_start_year }} - {{ issue_end_year }}</h6>
 
-                  <br />
+                    <br />
 
-                  <div>
-                    <input
-                      class="form-check-input me-2"
-                      type="checkbox"
-                      :value="value"
-                      :checked="formData[name].includes(value)"
-                      @change="updateSelection(value)"
-                    />
+                    <div>
+                      <input
+                        class="form-check-input me-2"
+                        type="checkbox"
+                        :value="value"
+                        :checked="formData[name].includes(value)"
+                        @change="updateSelection(value)"
+                      />
 
-                    <label class="form-check-label">
-                      {{ label || value }}</label
-                    >
+                      <label class="form-check-label">
+                        {{ label || value }}</label
+                      >
+                    </div>
                   </div>
+
+                  <h6 v-else>
+                    Select Required Fields Above For This To Show Options
+                  </h6>
                 </div>
 
-                <h6 v-else>
-                  Select Required Fields Above For This To Show Options
-                </h6>
+                <div class="row my-3 mt-5">
+                  <h6>Selected Options</h6>
+
+                  <div
+                    v-if="formData.collectedCurrencies.length > 0"
+                    v-for="{
+                      front_image,
+                      back_image,
+                      label,
+                      value,
+                      issue_start_year,
+                      issue_end_year,
+                    } of allOptions"
+                    :key="value"
+                    class="col-12 col-sm-6 col-md-4 col-lg-2 text-center"
+                  >
+                    <img :src="front_image" class="mb-3" />
+                    <img :src="back_image" class="mb-3" />
+
+                    <br />
+
+                    <h6>{{ issue_start_year }} - {{ issue_end_year }}</h6>
+
+                    <br />
+
+                    <div>
+                      <input
+                        class="form-check-input me-2"
+                        type="checkbox"
+                        :value="value"
+                        :checked="formData[name].includes(value)"
+                        @change="updateSelection(value)"
+                      />
+
+                      <label class="form-check-label">
+                        {{ label || value }}</label
+                      >
+                    </div>
+                  </div>
+
+                  <h6 v-else>Noting Selected So Far</h6>
+                </div>
               </div>
 
               <input
@@ -116,11 +165,20 @@
       <div class="mt-4 btn-group float-end">
         <button
           type="button"
+          @click="clearSelections"
+          class="btn btn-outline-success"
+        >
+          Clear Selections
+        </button>
+
+        <button
+          type="button"
           @click="resetForm"
           class="btn btn-outline-success"
         >
           Reset
         </button>
+
         <button type="submit" class="btn btn-success">Submit</button>
       </div>
     </form>
@@ -149,12 +207,16 @@ const defaultFormValues = {
 
 const formData = reactive(defaultFormValues);
 
+const clearSelections = () => {
+  formData.collectedCurrencies = [];
+};
+
 const resetForm = () => {
+  clearSelections();
   formData.continent = "";
   formData.zoneName = "";
   formData.type = "";
   formData.value = "";
-  formData.collectedCurrencies = [];
   formData.date = new Date().toISOString().split("T")[0];
   formData.note = "";
 };
@@ -274,39 +336,57 @@ const formInputs = () => [
         label: "Currencies To Add",
         required: true,
         type: "checklist-photo",
-        options:
-          formData.value || formData.collectedCurrencies
-            ? dbData.currencies
-                .filter(
-                  ({ type, value, id, country_id, name }) =>
-                    formData.collectedCurrencies.includes(id) ||
-                    (type == formData.type &&
-                      formData.value == value + " " + name &&
-                      country_id == formData.zoneName &&
-                      type == formData.type)
-                )
-                .sort((a, b) =>
-                  a.issue_start_year < b.issue_start_year ? -1 : 1
-                )
-                .map(
-                  ({
-                    id,
-                    name,
-                    value,
-                    front_image,
-                    back_image,
-                    issue_start_year,
-                    issue_end_year,
-                  }) => ({
-                    front_image: front_image || back_image,
-                    back_image: back_image || front_image,
-                    issue_start_year,
-                    issue_end_year,
-                    value: id,
-                    label: value + " " + name,
-                  })
-                )
-            : [],
+        allOptions: dbData.currencies
+          .filter(({ id }) => formData.collectedCurrencies.includes(id))
+          .map(
+            ({
+              id,
+              name,
+              value,
+              front_image,
+              back_image,
+              issue_start_year,
+              issue_end_year,
+            }) => ({
+              front_image: front_image || back_image,
+              back_image: back_image || front_image,
+              issue_start_year,
+              issue_end_year,
+              value: id,
+              label: value + " " + name,
+            })
+          ),
+        options: formData.value
+          ? dbData.currencies
+              .filter(
+                ({ type, value, id, country_id, name }) =>
+                  type == formData.type &&
+                  formData.value == value + " " + name &&
+                  country_id == formData.zoneName &&
+                  type == formData.type
+              )
+              .sort((a, b) =>
+                a.issue_start_year < b.issue_start_year ? -1 : 1
+              )
+              .map(
+                ({
+                  id,
+                  name,
+                  value,
+                  front_image,
+                  back_image,
+                  issue_start_year,
+                  issue_end_year,
+                }) => ({
+                  front_image: front_image || back_image,
+                  back_image: back_image || front_image,
+                  issue_start_year,
+                  issue_end_year,
+                  value: id,
+                  label: value + " " + name,
+                })
+              )
+          : [],
       },
     ],
   },
