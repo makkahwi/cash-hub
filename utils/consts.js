df0d851fc0106a7eda4c0a5e3ad35d3277ccd5a2
Makkahwi <SuhaibAhmadAi@hotmail.com>
2025-02-12 16:20:17 +0300

    Link Continents Const To DB Data

--- utils/consts.js
+++ utils/consts.js
@@ -1,4 +1,6 @@
-export const continents = ["Africa", "Americas", "Asia", "Europe", "Oceania"];
+import dbData from "@/api/db.json";
+
+export const continents = dbData.continents.map(({ name }) => name); // ["Africa", "Americas", "Asia", "Europe", "Oceania"];
 
 export const photoPlaceholder =
   "https://t3.ftcdn.net/jpg/02/73/18/48/360_F_273184815_uOmhKYLSlx3hQWNsGciu1EfbKrf7g5bQ.jpg";
