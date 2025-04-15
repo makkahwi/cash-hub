a03c69e7a469f94035fee9b4144b9a2a2ad10085
Makkahwi <SuhaibAhmadAi@hotmail.com>
2025-04-15 15:18:41 +0300

    Add ShowMore To Showroom

--- pages/showroom/List/index.vue
+++ pages/showroom/List/index.vue
@@ -18,22 +18,28 @@
       </div>
 
       <TableView
-        v-if="type.value == 'table'"
-        :currencies="currencies"
+        v-if="type.value === 'table'"
+        :currencies="visibleCurrencies"
         :notShowroom="notShowroom"
       />
 
       <ListView
-        v-if="type.value == 'list'"
-        :currencies="currencies"
+        v-if="type.value === 'list'"
+        :currencies="visibleCurrencies"
         :notShowroom="notShowroom"
       />
 
       <GridView
-        v-if="type.value == 'grid'"
-        :currencies="currencies"
+        v-if="type.value === 'grid'"
+        :currencies="visibleCurrencies"
         :notShowroom="notShowroom"
       />
+
+      <div class="text-center mt-4" v-if="currencies.length > visibleCurrencies.length">
+        <button class="btn btn-outline-primary" @click="showMore">
+          Show More
+        </button>
+      </div>
     </div>
   </PageSection>
 </template>
@@ -43,8 +49,9 @@ import PageSection from "@/components/PageSection";
 import GridView from "./Grid";
 import ListView from "./List";
 import TableView from "./Table";
-import { reactive } from "vue";
+import { reactive, ref, computed } from "vue";
 
+// View Type Handling
 const type = reactive({
   value: "table",
   update(newView) {
@@ -52,14 +59,26 @@ const type = reactive({
   },
 });
 
-defineProps({
+// Props
+const props = defineProps({
   currencies: {
     type: Array,
-    default: [],
+    default: () => [],
   },
   notShowroom: {
     type: Boolean,
     required: false,
   },
 });
+
+// Show Count Logic
+const showCount = ref(20); // start with 20
+
+const visibleCurrencies = computed(() => {
+  return props.currencies.slice(0, showCount.value);
+});
+
+const showMore = () => {
+  showCount.value += 20;
+};
 </script>
